fcb0c840beab92f93ee982380018df6e1ccad4a0
heman chandra <hemanchandra701@gmail.com>
2026-05-31 17:39:42 +0530

    jenkins

--- vars/nodeJS.groovy
+++ vars/nodeJS.groovy
@@ -1,81 +1,72 @@
-def call (Map configMap) {
+def call(Map configMap) {
+
     pipeline {
-    agent {
-        node {
-            label 'Agent-1'
-        }
-    }
-
-    environment { 
-        Course = 'Jenkins'
-        appVersion = ""
-        ACC_ID = "634758830486"
-        PROJECT = configMap.get('project')
-        COMPONENT = configMap.get('component')
-
-    }
-
-    options {
-        timeout(time: 60, unit: 'MINUTES') 
-        disableConcurrentBuilds()
-    }
-
-    stages {
-
-        stage('Read App Version') {
-            steps {
-                script {
-                    def packageJSON = readJSON file: 'package.json'
-                    appVersion = packageJSON.version
-                    echo "App Version is: ${appVersion}"
-                } 
+        agent {
+            node {
+                label 'Agent-1'
             }
         }
 
-        stage('Install Dependencies') {
-            steps {
-                script {
-                    sh """
-                    npm install 
-                    """
-                }
-            }
+        environment {
+            Course = 'Jenkins'
+            appVersion = ""
+            ACC_ID = "634758830486"
+            PROJECT = configMap.get('project')
+            COMPONENT = configMap.get('component')
         }
 
+        options {
+            timeout(time: 60, unit: 'MINUTES')
+            disableConcurrentBuilds()
+        }
 
+        stages {
 
-        stage('Build Image ECR') {
-            steps {
-                script {
-                    withAWS(region:'us-east-1',credentials:'aws-creds') {
-                        sh """
-                        aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                        docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
-                        docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
-                        """
+            stage('Read App Version') {
+                steps {
+                    script {
+                        def packageJSON = readJSON file: 'package.json'
+                        appVersion = packageJSON.version
+                        echo "App Version is: ${appVersion}"
                     }
-                    
+                }
+            }
+
+            stage('Install Dependencies') {
+                steps {
+                    sh "npm install"
+                }
+            }
+
+            stage('Build Image ECR') {
+                steps {
+                    script {
+                        withAWS(region: 'us-east-1', credentials: 'aws-creds') {
+                            sh """
+                            aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                            docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                            docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                            """
+                        }
+                    }
                 }
             }
         }
-    }
 
-
-    }
-
-    post { 
-        always { 
-            echo 'I will always say Hello again!'
-            cleanWs()
-        }
-        success {
-            echo 'I will run if success'
-        }
-        failure {
-            echo 'I will not run if failure'
-        }
-        aborted {
-            echo 'Pipeline is aborted'
+        post {
+            always {
+                echo 'I will always say Hello again!'
+                cleanWs()
+            }
+            success {
+                echo 'I will run if success'
+            }
+            failure {
+                echo 'I will not run if failure'
+            }
+            aborted {
+                echo 'Pipeline is aborted'
+            }
         }
     }
 }
